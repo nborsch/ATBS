1602afd7ea160acbf27c4ea1a54bc39d87505642
Nadia Borsch <misc@nborsch.com>
2018-05-21 16:39:30 -0500

    Add regexes for upper/lower case and digit checking

--- strongPass.py
+++ strongPass.py
@@ -7,12 +7,15 @@
 # 
 # Nadia Borsch      misc@nborsch.com        2018
 
-
 import re
 
 print('Welcome to the Strong Password Detection Program!')
 
-password = input('Please enter the password you want to check.\n')
+password = input('Please enter the password you want to check.\n\n')
+
+upperLower = re.compile(r'[A-Z]+[a-z]+')
+
+digit = re.compile(r'[0-9]+')
 
 # TODO: check if password is at least 8 characters long
 
