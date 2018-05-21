69a1b8e37e6ede4d12b46d190a4bc94303591db7
Nadia Borsch <misc@nborsch.com>
2018-05-21 16:26:59 -0500

    Ask for user input

---
+++ strongPass.py
@@ -0,0 +1,21 @@
+#! python3
+# -*- coding: utf-8 -*-
+#
+# Solution to Practice Project "Strong Password Detection"
+# from "Automate The Boring Stuff", by Al Sweigart,
+# Chapter 7.
+# 
+# Nadia Borsch      misc@nborsch.com        2018
+
+
+import re
+
+print('Welcome to the Strong Password Detection Program!')
+
+password = input('Please enter the password you want to check.\n')
+
+# TODO: check if password is at least 8 characters long
+
+# TODO: check if password contains both uppercase and lowercase characters
+
+# TODO check if password has at least one digit
