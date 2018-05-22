9882e641584b7754b8820b5702daf26e1de37de9
Nadia Borsch <misc@nborsch.com>
2018-05-22 13:48:57 -0500

    Add functionality

--- regexStrip.py
+++ regexStrip.py
@@ -7,8 +7,28 @@
 # 
 # Nadia Borsch      misc@nborsch.com        May/2018
 
-# Get user input
-print('Regex Version of strip()')
-stringIn = ('Please enter the string you want to strip.\n')
-stripIn = ('Please enter the string you want to use to do the stripping.\n')
+import re
 
+def regexStrip(s, chars):
+    if not chars:
+        left = re.compile(r'^\s*')
+        right = re.compile(r'\s*$')
+
+        s = left.sub('', s)
+        s = right.sub('', s)
+        
+    else:
+        left = re.compile(r'^[' + re.escape(chars) + r']*')
+        right = re.compile(r'[' + re.escape(chars) + r']*$')
+        
+        s = left.sub('', s)
+        s = right.sub('', s)
+    
+    return s
+
+# Start program and obtain user input
+print('Regex Version of strip()')
+user_string = input('Please enter the string you want to strip.\n')
+stripper = input('Please enter the string you want to use to do the stripping or leave it blank to strip all whitespaces.\n')
+
+print(regexStrip(user_string, stripper))
