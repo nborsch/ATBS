92aa97e4adbbfc2e4ae229f46ef5c11b50e16306
Nadia Borsch <misc@nborsch.com>
2018-06-10 10:51:28 -0500

    Reviewed and modified many aspects of schedComicDown.py

--- schedComicDown.py
+++ schedComicDown.py
@@ -18,16 +18,24 @@
 # 
 # Nadia Borsch      misc@nborsch.com        Jun/2018
 
-import csv, bs4, datetime, os, requests, threading, time
+import csv
+import os
+import threading
+import time
 
-def downloader(title, base_url, element, comic_img=False, check=True):
+from bs4 import BeautifulSoup
+import datetime
+import requests
+
+def downloader(title, base_url, element, check=True):
     """Downloads latest web comics"""
     res = requests.get(base_url)
     res.raise_for_status()
 
     # Find current comic image URL
-    soup = bs4.BeautifulSoup(res.text, "lxml")
+    soup = BeautifulSoup(res.text, "lxml")
     comic_elem = soup.select(element)
+
     if comic_elem == []:
         # Skip comic, not found
         print(f"Could not find current comic for {title}.")
@@ -36,27 +44,24 @@ def downloader(title, base_url, element, comic_img=False, check=True):
         comic_filename = os.path.basename(comic_url)
 
         # Check current comic file against saved file
-        if check == True and comic_filename in last_visit_data.values():
+        if check and comic_filename in last_visit_data.values():
             # No updates have been found
             print(f"No updates for {title}.")
-            pass
         else:
             # Get and save comic
             print(f"Downloading latest comic from {title}...")
-            if comic_img == True:
+
+            if not comic_url.startswith("http://"):
                 # URL for comic image needs a base URL
                 comic_url = f"{base_url}/{comic_url}"
-                res = requests.get(comic_url)
-                res.raise_for_status()
-            else:
-                # URL for comic image comes complete from src attribute
-                res = requests.get(comic_url)
-                res.raise_for_status()
 
-            comic_file = open(f"{folder}\{comic_filename}", "wb")
-            for part in res.iter_content(100000):
-                comic_file.write(part)
-            comic_file.close()
+            # URL for comic image comes complete from src attribute
+            res = requests.get(comic_url)
+            res.raise_for_status()
+
+            with open(f"{folder}\{comic_filename}", "wb") as comic_file:
+                for part in res.iter_content(100000):
+                    comic_file.write(part)
 
             # Log filename
             current_visit_data[title] = comic_filename
@@ -78,26 +83,20 @@ print("Comics will be saved on your Desktop under 'Updated Web Comics'.\n")
 
 # Open CSV file that stores last visit's dates
 try:
-    last_visit_file = open("schedComicDown.csv")
-    last_visit_reader = csv.reader(last_visit_file)
+    with open("schedComicDown.csv") as last_visit_file:
+        last_visit_reader = csv.reader(last_visit_file)
 
-    # Retrieve last visit data from file
-    last_visit_data = {}
-    for row in last_visit_reader:
-        last_visit_data[row[0]] = row[1]
-
-    last_visit_file.close()
+        # Retrieve last visit data from file
+        last_visit_data = {}
+        for row in last_visit_reader:
+            last_visit_data[row[0]] = row[1]
 
 except FileNotFoundError:
     last_visit_data = {}
 
-# Save current date on file
-current_visit_file = open("schedComicDown.csv", "w", newline="")
-current_visit_writer = csv.writer(current_visit_file)
-
 # Create folder on desktop to save all files
-current_date = datetime.datetime.now()
-current_date = current_date.strftime("%Y-%m-%d")
+current_date = datetime.datetime.now().strftime("%Y-%m-%d")
+
 folder = f"C:{os.path.join(os.environ['HOMEPATH'], 'Desktop')}\\{current_date} Web Comics"
 os.makedirs(folder, exist_ok=True)
 
@@ -114,7 +113,7 @@ comics = [
     ["Extra Ordinary", "http://www.exocomics.com/", "a.comic img"],
     ["Wonderella", "http://nonadventures.com/", "div#comic img"],
     ["Moonbeard", "http://moonbeard.com/", "div#comic div a img"],
-    ["Happle Tea", "http://www.happletea.com/", "div#comic img"]
+    ["Happle Tea", "http://www.happletea.com/", "div#comic img"],
 ]
 
 # Create and start thread objects
@@ -131,9 +130,12 @@ for download in downloads:
 # Add current comic updates to csv file
 updated_data = {**last_visit_data, **current_visit_data}
 
-for key, value in updated_data.items():
-    current_visit_writer.writerow([key, value])
-current_visit_file.close()
+# Save current date on file
+with open("schedComicDown.csv", "w", newline="") as current_visit_file:
+    current_visit_writer = csv.writer(current_visit_file)
+
+    for key, value in updated_data.items():
+        current_visit_writer.writerow([key, value])
 
 print("All done!")
 
